a24529b2fe326b4ea375fdf992fa3d2734e40a08
Csaba Agosthazy <40959918+csabaagosthazy@users.noreply.github.com>
2025-09-11 14:31:08 +0200

    Dark mode detection and theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './context/RouterProvider';
 import SplashScreen from './pages/SplashScreen';
 import { getInitialLocale } from './utils/localeUtils';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 function App() {
   const [loading, setLoading] = useState(true);
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       {loading && (
         <SplashScreen
           onFinish={() => setLoading(false)}
@@ -17,7 +33,7 @@ function App() {
         />
       )}
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
 }
 
